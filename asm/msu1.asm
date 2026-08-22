dcaf4fc4e42c63aa38eee74b656d1c11873880f2
LuigiBlood <luigiblood@gmail.com>
2026-08-22 02:59:23 +0200

    Fix MSU1 code, retime intro, start clock upon pressing Start

--- asm/msu1.asm
+++ asm/msu1.asm
@@ -1,18 +1,21 @@
 enqueue pc
 seekAddr($80A2AF)
-//Hijack Game Mode $10
+//Hijack Sub Game Mode $10
 	jsl hack_msu_play
 	//lda.w $1972
 	//asl
 dequeue pc
 
 hack_msu_play:
-	lda.w #0
+	sep #$20
+	lda.w MSU_STATUS
+	bit.b #%00010000
+	bne +
+
+	rep #$20
+	lda.w #1
 	sta.w MSU_TRK
 	sep #$20
-	lda.w MSU_ID5
-	cmp.b #$01
--;	bne -
 -;	bit.w MSU_STATUS
 	bvs -
 	lda.b #$FF
@@ -20,7 +23,16 @@ hack_msu_play:
 	lda.b #$01
 	sta.w MSU_PLAY
 
-	rep #$20
++;	rep #$20
 	lda.w $1972
 	asl
+	rtl
+
+msu_init:
+	php
+	rep #$20
+	lda.w #0
+	sta.w MSU_TRK
+	sta.w MSU_VOL
+	plp
 	rtl
--- asm/time.asm
+++ asm/time.asm
@@ -9,15 +9,63 @@ dequeue pc
 //Change Hour and Minute (At Boot) (Original Code: 18:05)
 enqueue pc
 seekAddr($808023)
+	jsl time_init
+	nop; nop
+	nop; nop
+	nop; nop; nop; nop
+dequeue pc
+time_init:
+	lda.b #0
+	sta.l $7FFFF4	//Stop the clock by default
 	lda.b #18
 	sta.l $7FFFF9
 	lda.b #2
 	sta.l $7FFFFF
-dequeue pc
+	rep #$20
+	lda.w #60*3
+	sta.l $7FFFFA
+	jsl msu_init
+	sep #$20
+	rtl
+
 
 //Change Minute (Before Waiting Screen) (Original Code xx:06)
 enqueue pc
 seekAddr($809BF7)
 	lda.b #3
 	sta.l $7FFFFF
+dequeue pc
+
+//Add ability to pause/unpause the clock
+enqueue pc
+seekAddr($80F8D1)
+	php
+	sep #$20
+	lda.l $7FFFF4
+	beq +
+	jml $80F969
++;	plp
+	rtl
+dequeue pc
+
+//Unpause the clock when pressing Start in the Controls select
+enqueue pc
+seekAddr($80A25D)
+	jsl hack_controls_startclock
+	nop
+dequeue pc
+hack_controls_startclock:
+	lda.b #$01
+	sta.l $7FFFF4
+	sta.w $1DE3
+	rtl
+
+
+//Retime Intro
+enqueue pc
+seekAddr($80A33C)
+	dw 43*60,22*60,35*60,60*60
+	dw 4,4,5,6
+	//dw $0A8C,$0564,$08AC,$0E10	//dw 45*60,23*60,37*60,60*60
+	//dw $0004,$0004,$0005,$0006
 dequeue pc
