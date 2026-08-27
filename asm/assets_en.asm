b0cade3f4f27d3d377f61806bb79278774d9be99
LuigiBlood <luigiblood@gmail.com>
2026-08-27 23:41:47 +0200

    Translate Pause menu

--- asm/assets_en.asm
+++ asm/assets_en.asm
@@ -63,12 +63,43 @@ db $01,$F1,$00,$03
 db $46,$09,$47,$09
 db $FF
 
+
+// Change Pause Menu
+enqueue pc
+seekAddr($84D463+1); dw oam_tbl_pause
+seekAddr($84D468+1); dw oam_tbl_pause>>16
+seekAddr($84D46D+1); dw oam_tbl_pause_size
+dequeue pc
+
+oam_tbl_pause:	//$84C407
+insert "../roms/bs_supermariousa_ep1.bs",0x24407,0x13B
+db $58,$70,$27,$35,$00	//E
+db $60,$70,$28,$35,$00	//X
+db $68,$70,$29,$35,$00	//I
+db $70,$70,$2A,$35,$00	//T
+
+db $58,$78,$23,$35,$02	//ST
+db $68,$80,$35,$35,$00	//A
+db $70,$80,$36,$35,$00	//G
+db $78,$80,$37,$35,$00	//E
+db $80,$80,$38,$35,$00	//
+db $88,$80,$39,$35,$00	//S
+db $90,$80,$3A,$35,$00	//E
+db $98,$80,$3B,$35,$00	//L
+db $A0,$80,$3C,$35,$00	//E
+db $A8,$78,$2D,$35,$02	//CT
+insert "../roms/bs_supermariousa_ep1.bs",0x24579,0xAF
+oam_tbl_pause_end:
+constant oam_tbl_pause_size = oam_tbl_pause_end-oam_tbl_pause
+
 //Include all graphics
 enqueue pc
 seekAddr($89A800); insert "../gfx/en/controls.map"
 seekAddr($99A800); insert "../gfx/en/controls.chr"
 seekAddr($99B400); insert "../gfx/temp/controls_text.chr",0x000,0xC00
 seekAddr($99C800); insert "../gfx/temp/controls_text.chr",0xC00
+
+seekAddr($93C000); insert "../gfx/en/pause_menu.chr"
 
 //Change Size for last controls_text.chr part
 seekAddr($83E8E8); dw 0x0C00
